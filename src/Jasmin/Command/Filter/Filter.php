659cdafc728f02c97b4c7293e32f705eb8047225
BlackOrder <4302157+BlackOrder@users.noreply.github.com>
2022-11-15 00:29:17 +0200

    format Filter class

--- src/Jasmin/Command/Filter/Filter.php
+++ src/Jasmin/Command/Filter/Filter.php
@@ -5,77 +5,73 @@ namespace JasminWeb\Jasmin\Command\Filter;
 use JasminWeb\Jasmin\Command\AddValidator;
 use JasminWeb\Jasmin\Command\BaseCommand;
 
-class Filter extends BaseCommand
-{
-    public const TRANSPARENT = 'transparentfilter';
-    public const USER = 'userfilter';
-    public const CONNECTOR = 'connectorfilter';
+class Filter extends BaseCommand {
+  public const TRANSPARENT = 'transparentfilter';
+  public const USER = 'userfilter';
+  public const CONNECTOR = 'connectorfilter';
 
-    /**
-     * @return AddValidator
-     */
-    protected function getAddValidator(): AddValidator
-    {
-        return new FilterAddValidator();
+  /**
+   * @return AddValidator
+   */
+  protected function getAddValidator(): AddValidator {
+    return new FilterAddValidator();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getName(): string {
+    return 'filter';
+  }
+
+  /**
+   * @param array $exploded
+   * @return array
+   */
+  protected function parseList(array $exploded): array
+  {
+    $filters = [];
+    foreach ($exploded as $expl) {
+      $filter = trim($expl);
+
+      $ff = strstr($expl, 'Total Filters:', true);
+      if (!empty($ff)) {
+        $filter = trim($ff);
+      }
+
+      $temp_filter = explode(' ', $filter);
+      $temp_filter = array_filter($temp_filter);
+
+      $fixed_connector = [];
+      foreach ($temp_filter as $temp) {
+        $fixed_connector[] = $temp;
+      }
+
+      $row = (object) [
+        'fid' => $fixed_connector[0],
+        'type' => $fixed_connector[1],
+        'description' => substr($filter, strpos($filter, '<'), strpos($filter, '>')),
+        'routes' => [],
+      ];
+
+      if (false !== strpos($filter, 'MT')) {
+        $row->routes[] = 'MT';
+      }
+
+      if (false !== strpos($filter, 'MO')) {
+        $row->routes[] = 'MO';
+      }
+
+      $filters[] = $row;
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    protected function getName(): string
-    {
-        return 'filter';
-    }
+    return $filters;
+  }
 
-    /**
-     * @param array $exploded
-     * @return array
-     */
-    protected function parseList(array $exploded): array
-    {
-        $filters = [];
-        foreach ($exploded as $expl) {
-            $filter = trim($expl);
-
-            $ff = strstr($expl, 'Total Filters:', true);
-            if (!empty($ff)) {
-                $filter = trim($ff);
-            }
-
-            $temp_filter = explode(' ', $filter);
-            $temp_filter = array_filter($temp_filter);
-
-            $fixed_connector = [];
-            foreach ($temp_filter as $temp) {
-                $fixed_connector[] = $temp;
-            }
-
-            $row = (object) [
-                'fid' => $fixed_connector[0],
-                'type' => $fixed_connector[1],
-                'description' => substr($filter, strpos($filter, '<'), strpos($filter, '>')),
-                'routes' => [],
-            ];
-
-            if (false !== strpos($filter, 'MT')) {
-                $row->routes[] = 'MT';
-            }
-
-            if (false !== strpos($filter, 'MO')) {
-                $row->routes[] = 'MO';
-            }
-
-            $filters[] = $row;
-        }
-
-        return $filters;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    protected function isNeedPersist(): bool
-    {
-        return true;
-    }
+  /**
+   * {@inheritdoc}
+   */
+  protected function isNeedPersist(): bool {
+    return true;
+  }
 }
