7bc636fe3325f9ec5db0c1fcf10f78f64cd22d09
BlackOrder <4302157+BlackOrder@users.noreply.github.com>
2022-11-15 00:18:25 +0200

    format http connector class

--- src/Jasmin/Command/HttpConnector/Connector.php
+++ src/Jasmin/Command/HttpConnector/Connector.php
@@ -5,62 +5,57 @@ namespace JasminWeb\Jasmin\Command\HttpConnector;
 use JasminWeb\Jasmin\Command\AddValidator;
 use JasminWeb\Jasmin\Command\BaseCommand;
 
-class Connector extends BaseCommand
-{
-    /**
-     * @return AddValidator
-     */
-    protected function getAddValidator(): AddValidator
-    {
-        return new HttpConnectorAddValidator();
+class Connector extends BaseCommand {
+  /**
+   * @return AddValidator
+   */
+  protected function getAddValidator(): AddValidator {
+    return new HttpConnectorAddValidator();
+  }
+
+  protected function getName(): string {
+    return 'httpccm';
+  }
+
+  /**
+   * @param array $exploded
+   * @return array
+   */
+  protected function parseList(array $exploded): array
+  {
+    $connectors = [];
+    foreach ($exploded as $expl) {
+      $row = trim($expl);
+
+      $ff = strstr($expl, 'Total Httpccs:', true);
+      if (!empty($ff)) {
+        $row = trim($ff);
+      }
+
+      $temp_row = explode(' ', $row);
+      $temp_row = array_filter($temp_row);
+
+      $fixed_row = array();
+      foreach ($temp_row as $temp) {
+        $fixed_row[] = $temp;
+      }
+
+      $connectors[] = (object) [
+        'cid' => $fixed_row[0],
+        'type' => $fixed_row[1],
+        'method' => $fixed_row[2],
+        'url' => $fixed_row[3],
+      ];
     }
 
-    protected function getName(): string
-    {
-        return 'httpccm';
-    }
+    return $connectors;
+  }
 
-    /**
-     * @param array $exploded
-     * @return array
-     */
-    protected function parseList(array $exploded): array
-    {
-        $connectors = [];
-        foreach ($exploded as $expl) {
-            $row = trim($expl);
+  protected function isHeavy(): bool {
+    return true;
+  }
 
-            $ff = strstr($expl, 'Total Httpccs:', true);
-            if (!empty($ff)) {
-                $row = trim($ff);
-            }
-
-            $temp_row = explode(' ', $row);
-            $temp_row = array_filter($temp_row);
-
-            $fixed_row = array();
-            foreach ($temp_row as $temp){
-                $fixed_row[] = $temp;
-            }
-
-            $connectors[] = (object) [
-                'cid' => $fixed_row[0],
-                'type' => $fixed_row[1],
-                'method' => $fixed_row[2],
-                'url' => $fixed_row[3],
-            ];
-        }
-
-        return $connectors;
-    }
-
-    protected function isHeavy(): bool
-    {
-        return true;
-    }
-
-    protected function isNeedPersist(): bool
-    {
-        return true;
-    }
+  protected function isNeedPersist(): bool {
+    return true;
+  }
 }
