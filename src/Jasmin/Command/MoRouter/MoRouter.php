60f558d556e852a719ce3c40556fe1965eed931a
BlackOrder <4302157+BlackOrder@users.noreply.github.com>
2022-11-15 00:13:02 +0200

    format MoRouter class

--- src/Jasmin/Command/MoRouter/MoRouter.php
+++ src/Jasmin/Command/MoRouter/MoRouter.php
@@ -5,85 +5,79 @@ namespace JasminWeb\Jasmin\Command\MoRouter;
 use JasminWeb\Jasmin\Command\AddValidator;
 use JasminWeb\Jasmin\Command\BaseCommand;
 
-class MoRouter extends BaseCommand
-{
-    public const STATIC = 'StaticMORoute';
-    public const DEFAULT = 'DefaultRoute';
-    public const RANDOM = 'RandomRoundrobinMORoute';
-    public const FAILOVER = 'FailoverMORoute';
+class MoRouter extends BaseCommand {
+  public const STATIC  = 'StaticMORoute';
+  public const DEFAULT = 'DefaultRoute';
+  public const RANDOM = 'RandomRoundrobinMORoute';
+  public const FAILOVER = 'FailoverMORoute';
 
-    /**
-     * @return AddValidator
-     */
-    protected function getAddValidator(): AddValidator
-    {
-        return new MoRouterBaseAddValidator();
+  /**
+   * @return AddValidator
+   */
+  protected function getAddValidator(): AddValidator {
+    return new MoRouterBaseAddValidator();
+  }
+
+  protected function getName(): string {
+    return 'morouter';
+  }
+
+  /**
+   * @param array $exploded
+   * @return array
+   */
+  protected function parseList(array $exploded): array
+  {
+    $routers = [];
+    foreach ($exploded as $expl) {
+      $router = trim($expl);
+
+      $ff = strstr($expl, 'Total MO Routes:', true);
+      if (!empty($ff)) {
+        $router = trim($ff);
+      }
+
+      $router = preg_replace(['/\s{2,}/', '/(<\w)(\s)?/'], [' ', '$1'], $router);
+      $fixed_routers = explode(' ', $router);
+
+      $row = (object) [
+        'order' => (int) array_shift($fixed_routers),
+        'type' => array_shift($fixed_routers),
+        'connectors' => [],
+        'filters' => [],
+      ];
+
+      //Get all http connectors
+      preg_match_all('~http\((.*?)\)~', $router, $MOhttpConnectors);
+
+      //Get all smpps connectors
+      preg_match_all('~smpps\((.*?)\)~', $router, $MOsmppsConnectors);
+      $row->connectors = array_merge($MOhttpConnectors[0], $MOsmppsConnectors[0]);
+
+      //Get all filters
+      preg_match_all('~<(.*?)>~', $router, $MTfilters);
+      $row->filters = $MTfilters[0];
+
+      $routers[] = $row;
     }
 
-    protected function getName(): string
-    {
-        return 'morouter';
+    return $routers;
+  }
+
+  protected function isHeavy(): bool {
+    return true;
+  }
+
+  public function prepareAttributes(array $data): array
+  {
+    if (isset($data['filters']) && !empty($data['filters'])) {
+      $data['filters'] = implode(';', $data['filters']);
     }
 
-    /**
-     * @param array $exploded
-     * @return array
-     */
-    protected function parseList(array $exploded): array
-    {
-        $routers = [];
-        foreach ($exploded as $expl) {
-            $router = trim($expl);
-
-            $ff = strstr($expl, 'Total MO Routes:', true);
-            if (!empty($ff)) {
-                $router = trim($ff);
-            }
-
-            $router = preg_replace(['/\s{2,}/', '/(<\w)(\s)?/'], [' ','$1'], $router);
-            $fixed_routers = explode(' ', $router);
-
-            $row = (object) [
-                'order'     => (int)array_shift($fixed_routers),
-                'type'      => array_shift($fixed_routers),
-                'connectors' => [],
-                'filters'   => [],
-            ];
-
-            
-            //Get all http connectors
-            preg_match_all('~http\((.*?)\)~', $router, $MOhttpConnectors);
-            
-            //Get all smpps connectors
-            preg_match_all('~smpps\((.*?)\)~', $router, $MOsmppsConnectors);
-            $row->connectors = array_merge($MOhttpConnectors[0],$MOsmppsConnectors[0]);
-
-            //Get all filters
-            preg_match_all('~<(.*?)>~', $router, $MTfilters);
-            $row->filters = $MTfilters[0];
-
-
-            $routers[] = $row;
-        }
-
-        return $routers;
+    if (isset($data['connectors']) && !empty($data['filters'])) {
+      $data['connectors'] = implode(';', $data['connectors']);
     }
 
-    protected function isHeavy(): bool
-    {
-        return true;
-    }
-
-    public function prepareAttributes(array $data): array
-    {
-        if (isset($data['filters']) && !empty($data['filters'])) {
-            $data['filters'] = implode(';', $data['filters']);
-        }
-
-        if (isset($data['connectors']) && !empty($data['filters'])) {
-            $data['connectors'] = implode(';', $data['connectors']);
-        }
-
-        return $data;
-    }
+    return $data;
+  }
 }
