83ce8ff7c0875842c039eec6943d5743068421ab
BlackOrder <4302157+BlackOrder@users.noreply.github.com>
2022-11-15 14:19:07 +0200

    BaseCommand ShowTrait

--- src/Jasmin/Command/BaseCommand.php
+++ src/Jasmin/Command/BaseCommand.php
@@ -5,7 +5,7 @@ namespace JasminWeb\Jasmin\Command;
 use JasminWeb\Jasmin\Connection\Session;
 
 abstract class BaseCommand {
-  use ListTrait, AddTrait, RemoveTrait;
+  use ListTrait, AddTrait, ShowTrait, RemoveTrait;
 
   /**
    * @var Session
