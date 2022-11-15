c8fc32f959019eeb8929a4c087b2d753d8743b36
BlackOrder <4302157+BlackOrder@users.noreply.github.com>
2022-11-15 19:26:48 +0200

    showTrait parse

--- src/Jasmin/Command/ShowTrait.php
+++ src/Jasmin/Command/ShowTrait.php
@@ -20,5 +20,35 @@ trait ShowTrait {
    * @param array $exploded
    * @return object
    */
-  abstract protected function parseShow(array $exploded): object;
+  protected function parseShow(array $exploded): object {
+    $options = [];
+    foreach ($exploded as $row) {
+      $option = trim($row);
+
+      if (false !== strpos($option, 'jcli :')) {
+        continue;
+      }
+
+      $option = explode(' ', $option);
+      $value = array_pop($option);
+      $key = implode(' ', $option);
+
+      if (empty($key)) {
+        continue;
+      }
+
+      $key = explode(' ', $key);
+
+      while (count($key) > 1) {
+        $tmpKey = array_pop($key);
+        $value = [$tmpKey => $value];
+      }
+
+      $key = $key[0];
+
+      $options = array_merge_recursive($options, [$key => $value]);
+    }
+
+    return json_decode(json_encode($options));
+  }
 }
--- src/Jasmin/Command/User/User.php
+++ src/Jasmin/Command/User/User.php
@@ -68,42 +68,6 @@ class User extends BaseCommand {
   }
 
   /**
-   * @param array $exploded
-   * @return object
-   */
-  protected function parseShow(array $exploded): object {
-    $options = [];
-    foreach ($exploded as $row) {
-      $option = trim($row);
-
-      if (false !== strpos($option, 'jcli :')) {
-        continue;
-      }
-
-      $option = explode(' ', $option);
-      $value = array_pop($option);
-      $key = implode(' ', $option);
-
-      if (empty($key)) {
-        continue;
-      }
-
-      $key = explode(' ', $key);
-
-      while (count($key) > 1) {
-        $tmpKey = array_pop($key);
-        $value = [$tmpKey => $value];
-      }
-
-      $key = $key[0];
-
-      $options = array_merge_recursive($options, [$key => $value]);
-    }
-
-    return json_decode(json_encode($options));
-  }
-
-  /**
    * {@inheritdoc}
    */
   protected function isNeedPersist(): bool {
